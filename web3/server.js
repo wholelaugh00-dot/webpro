80adc7b7d2e08aaaad30aa9ef3ea6d5eb4d3513f
wholelaugh00-dot <wholelaugh00@gmail.com>
2026-04-26 14:21:13 -0400

    Add files via upload
    Added GET and POST endpoints

--- web3/server.js
+++ web3/server.js
@@ -1,7 +1,50 @@
 const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res) => {
-  res.end("Server works!");
-}).listen(3000, () => {
-  console.log("Server running");
+let movies = [];
+let id = 1;
+
+if (fs.existsSync("movies.json")) {
+  movies = JSON.parse(fs.readFileSync("movies.json"));
+}
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  // GET
+  if (req.method === "GET" && req.url === "/movies") {
+    res.end(JSON.stringify(movies));
+  }
+
+  // POST
+  else if (req.method === "POST" && req.url === "/movies") {
+    let body = "";
+
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const data = JSON.parse(body);
+
+      const newMovie = {
+        id: id++,
+        title: data.title
+      };
+
+      movies.push(newMovie);
+      fs.writeFileSync("movies.json", JSON.stringify(movies));
+
+      res.end(JSON.stringify(newMovie));
+    });
+  }
+
+  else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ message: "Not found" }));
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
